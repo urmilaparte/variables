ce355ec4dbe75e464cf0494d3d3fb7564ca6ebdb
urmilaparte <165538417+urmilaparte@users.noreply.github.com>
2024-07-14 20:07:10 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,65 @@
+const input = require("readline-sync");
+
+// Q1-Write a program to print 'Hello World' as output.
+
+// let str = "Hello World";
+// console.log(str);
+
+// Q2-Write a program to print your name as output.
+
+// let names = "Sandeep";
+// console.log(names);
+
+// Q3-Write a program to print three hashes '###' as output.
+
+// let x = "# # #";
+// console.log(x);
+
+// Q4-Write a program to print 5 as output.
+
+// let y = 5;
+// console.log(y);
+
+// Q5-Write a program to store your name in a variable and print it in  output
+// let s = "Dilip";
+// console.log(s);
+
+// Q6-Write a program to store your birth year in a variable and print it in output.
+
+// let year = 2004;
+// console.log(year);
+
+// Q7-Write a program to take three variable to store your birth date ,month,year,respectively and print one by one specific order.
+
+// let date = 6;
+// let month = "June";
+// let year = 2004;
+// console.log(date, month, year);
+
+// Q8-Write a program to take two inputs and prints those two inputs as output.
+
+// apple, mango
+// let  a = input.question("enter the name:-");
+// let  b = input.question('enter the name:-');
+
+// console.log(a,b);
+
+// Q9-Write a program to take two input first name and last name from the user and print name
+
+// Sandeep sunani
+
+// let first = input.question("enter first name:-");
+// let last =  input.question("enter the lastname:-");
+
+// console.log(first,last);
+
+// Q10-Write a program to take two value from the user and swap them.
+
+// let p = input.questionInt("enter the number:-");
+// let q = input.questionInt("enter the number:-");
+
+// p = p + q;
+// q = p - q;
+// p = p - q;
+
+// console.log(p, q);
